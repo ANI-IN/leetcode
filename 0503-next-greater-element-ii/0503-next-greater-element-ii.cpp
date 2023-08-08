195cced892fb08cc19eb8edf2ee5bea2fb8e3207
ANI-IN <44803072+ANI-IN@users.noreply.github.com>
2023-08-08 11:40:28 +0530

    Time: 34 ms (76.56%), Space: 24.9 MB (17.76%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -1,35 +1,32 @@
 class Solution {
 public:
-    vector<int> nextGreaterElements(vector<int>& nums) {
-        vector<int> ans;
-        int n=nums.size();
-        stack<int> st;
+    vector<int> nextGreaterElements(vector<int>& nums2) {
+        int n=nums2.size();
         
+        stack<int> st;
+        vector<int> ans;
         for(int i=2*n-1;i>=0;i--)
         {
-            while(!st.empty() && nums[i%n]>=st.top())
-            {
+            while(!st.empty() && nums2[i%n]>=st.top())
                 st.pop();
-            }
-             
-            if(i<n)
+            
+          if(i<n)
             {
-                if(!st.empty())
-                {
-                    ans.push_back(st.top());
-                }
-                else
-                {
-                    ans.push_back(-1);
-                }
+                if(st.empty())
+               ans.push_back(-1);
+            else
+               ans.push_back(st.top());
             }
-            st.push(nums[i%n]);
+            
+            st.push(nums2[i%n]);
         }
         reverse(ans.begin(),ans.end());
         
+        
+       
+        
         return ans;
-            
-            
+       
             
             
     }
