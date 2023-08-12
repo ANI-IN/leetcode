8ec57ab2512c462f93bf2102a928628a663b3bc4
ANI-IN <44803072+ANI-IN@users.noreply.github.com>
2023-08-12 10:32:37 +0530

    Time: 147 ms (71.24%), Space: 90.5 MB (18.46%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
@@ -1,29 +1,23 @@
 class Solution {
 public:
-    int numberOfSubarrays(vector<int>& nums, int goal) {
-        
-        for(int i=0;i<nums.size();i++)
-        {
-            nums[i]=nums[i]%2==0?0:1;
-        }
-        
-        for(auto it : nums)
-            cout<<it;
-        
+    int numberOfSubarrays(vector<int>& nums, int k) {
+        int n=nums.size();
         unordered_map<int,int>m;
-        int i=0,j=0,n=nums.size(),ans=0;
-        int sum=0;
         
+        int i=0,j=0;
+        int sum=0;
+        int curr=0;
         m[0]=1;
-        for(int i=0;i<n;i++)
-        {
-            sum+=nums[i];
-            if(m.find(sum-goal)!=m.end())
+        while(j<n)
+        {   
+            sum=sum+(nums[j]%2);
+            if(m.find(sum-k)!=m.end())
             {
-                ans+=m[sum-goal];
+                curr+=m[sum-k];
             }
             m[sum]++;
+            j++;
         }
-        return ans;
+        return curr;
     }
 };
