d594edb7585d138a9deea171c492d4e2c0ffa745
ANI-IN <44803072+ANI-IN@users.noreply.github.com>
2023-08-20 11:47:08 +0530

    Time: 2098 ms (14.77%), Space: 5.9 MB (39.96%) - LeetHub

---
+++ 0507-perfect-number/0507-perfect-number.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool checkPerfectNumber(int num) {
+        
+        int sum=0;
+        for(int i=1;i<=num-1;i++)
+        {
+            if(num%i==0)
+                sum+=i;
+        }
+        
+        return sum==num;
+    }
+};
