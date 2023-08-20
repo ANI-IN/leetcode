652b3cfa621126e3f55026605c9131ed787900dd
ANI-IN <44803072+ANI-IN@users.noreply.github.com>
2023-08-20 11:03:17 +0530

    Time: 7 ms (100.00%), Space: 7.5 MB (100.00%) - LeetHub

--- 2829-determine-the-minimum-sum-of-a-k-avoiding-array/2829-determine-the-minimum-sum-of-a-k-avoiding-array.cpp
+++ 2829-determine-the-minimum-sum-of-a-k-avoiding-array/2829-determine-the-minimum-sum-of-a-k-avoiding-array.cpp
@@ -1,22 +1,28 @@
 class Solution {
 public:
     int minimumSum(int n, int k) {
-        unordered_set<int> st;
-        int i=1;
-        int sum=0;
-        while(n)
+        vector<int> arr={1};
+        int i=2;
+        
+        bool t;
+        while(arr.size()<n)
         {
-            if(st.find(i)==st.end())
+            t=1;
+            for(auto it : arr)
             {
-                sum=sum+i;
-                
-                st.insert(k-i);
-                n--;
+                if(it+i==k)
+                {
+                    t=0;
+                    break;
+                }
             }
+            if(t)
+                arr.push_back(i);
             i++;
-            
-            
         }
+        int sum=0;
+        for(auto it : arr)
+            sum+=it;
         return sum;
     }
 };
