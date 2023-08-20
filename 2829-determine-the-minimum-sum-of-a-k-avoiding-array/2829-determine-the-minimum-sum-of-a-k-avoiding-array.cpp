3792d5f8e00aa948c8805fd517057ac31e300db6
ANI-IN <44803072+ANI-IN@users.noreply.github.com>
2023-08-20 11:04:55 +0530

    Time: 11 ms (100.00%), Space: 7.4 MB (100.00%) - LeetHub

--- 2829-determine-the-minimum-sum-of-a-k-avoiding-array/2829-determine-the-minimum-sum-of-a-k-avoiding-array.cpp
+++ 2829-determine-the-minimum-sum-of-a-k-avoiding-array/2829-determine-the-minimum-sum-of-a-k-avoiding-array.cpp
@@ -1,28 +1,43 @@
 class Solution {
 public:
     int minimumSum(int n, int k) {
-        vector<int> arr={1};
-        int i=2;
+        // Initialize an array 'arr' to store the sequence of integers.
+        vector<int> arr = {1};
         
-        bool t;
-        while(arr.size()<n)
-        {
-            t=1;
-            for(auto it : arr)
-            {
-                if(it+i==k)
-                {
-                    t=0;
+        // Initialize a variable 'i' to start generating integers from 2.
+        int i = 2;
+        
+        // Initialize a boolean variable 't' to track whether the current integer satisfies the condition.
+        bool check;
+        
+        // Loop until the 'arr' contains 'n' elements.
+        while (arr.size() < n) {
+            // Reset the 't' flag for each iteration.
+            check = true;
+            
+            // Check each integer 'it' in 'arr' to see if the sum of 'it' and 'i' equals 'k'.
+            for (auto it : arr) {
+                if (it + i == k) {
+                    // If the sum matches 'k', set 't' to false and break out of the loop.
+                    check = false;
                     break;
                 }
             }
-            if(t)
+            
+            // If 't' is true, the current integer 'i' can be added to 'arr'.
+            if (check)
                 arr.push_back(i);
+            
+            // Increment the integer 'i' for the next iteration.
             i++;
         }
-        int sum=0;
-        for(auto it : arr)
-            sum+=it;
+        
+        // Calculate the sum of all integers in 'arr'.
+        int sum = 0;
+        for (auto it : arr)
+            sum += it;
+        
+        // Return the calculated sum.
         return sum;
     }
 };
