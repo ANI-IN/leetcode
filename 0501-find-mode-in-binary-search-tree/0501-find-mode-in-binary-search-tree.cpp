df963ce0ec606eb3b56513e7d574946bd8a6b013
ANI-IN <44803072+ANI-IN@users.noreply.github.com>
2023-08-20 11:37:55 +0530

    Time: 23 ms (47.00%), Space: 25.8 MB (13.56%) - LeetHub

---
+++ 0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.cpp
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    unordered_map<int,int>m;
+    void solve(TreeNode *root)
+    {
+        if(root==NULL)
+            return;
+        m[root->val]++;
+        solve(root->left);
+        solve(root->right);
+    }
+    vector<int> findMode(TreeNode* root) {
+        priority_queue<pair<int,int>>q;
+        solve(root);
+        for(auto it : m)
+        {
+            q.push({it.second,it.first});
+        }
+        
+        int maxi=q.top().first;
+        vector<int> ans;
+        while(q.top().first==maxi && !q.empty())
+        {
+            ans.push_back(q.top().second);
+            q.pop();
+        }
+        return ans;
+            
+        
+    }
+};
