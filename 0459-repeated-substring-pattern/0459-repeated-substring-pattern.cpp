6a0d968048f0102563e85515038cb66975013a6e
ANI-IN <44803072+ANI-IN@users.noreply.github.com>
2023-08-21 14:25:05 +0530

    Time: 34 ms (33.55%), Space: 91.7 MB (20.17%) - LeetHub

---
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool repeatedSubstringPattern(string s) {
+        int n=s.size();
+        for(int i=1;i<=n/2;i++)
+        {
+            if(n%i==0)
+            {
+                int t=n/i;
+                string str=s.substr(0,i);
+                string tt="";
+                while(t--)
+                {
+                    tt+=str;
+                }
+                if(tt==s)
+                    return true;
+            }
+        }
+        return false;
+    }
+};
