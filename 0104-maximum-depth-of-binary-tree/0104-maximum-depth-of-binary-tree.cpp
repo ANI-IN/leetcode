323a131ff24f7c831aeaa3378fc509b1382dc1bd
ANI-IN <44803072+ANI-IN@users.noreply.github.com>
2023-08-22 15:45:33 +0530

    Time: 7 ms (69.47%), Space: 18.9 MB (8.63%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
@@ -12,30 +12,10 @@
 class Solution {
 public:
     int maxDepth(TreeNode* root) {
-        if (!root) {
-            return 0; // Base case: empty tree has depth 0
-        }
         
-        queue<TreeNode*> q;
-        q.push(root);
-
-        int ans = 0;
-
-        while (!q.empty()) {
-            int n = q.size();
-            for (int i = 0; i < n; i++) {
-                auto it = q.front();
-                q.pop();
-
-                if (it->left) {
-                    q.push(it->left);
-                }
-                if (it->right) {
-                    q.push(it->right);
-                }
-            }
-            ans++;
-        }
-        return ans;
+        if(root==NULL)
+            return 0;
+        
+        return 1+max(maxDepth(root->left),maxDepth(root->right));
     }
 };
