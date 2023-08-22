caaa6c924464bb884476f4c4c650e2b667cd94b6
ANI-IN <44803072+ANI-IN@users.noreply.github.com>
2023-08-22 17:44:15 +0530

    Time: 3 ms (39.57%), Space: 7.2 MB (76.84%) - LeetHub

---
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int numIdenticalPairs(vector<int>& nums) {
+        int n=nums.size();
+        int s=0;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=i+1;j<n;j++)
+            {
+                if(nums[i]==nums[j])
+                    s++;
+            }
+        }
+        return s;
+    }
+};
