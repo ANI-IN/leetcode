db84025e8a952be2857305182de4ab9fa8dec77a
ANI-IN <44803072+ANI-IN@users.noreply.github.com>
2023-08-22 17:45:41 +0530

    Time: 3 ms (39.57%), Space: 7.2 MB (32.59%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.cpp
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.cpp
@@ -3,13 +3,16 @@ public:
     int numIdenticalPairs(vector<int>& nums) {
         int n=nums.size();
         int s=0;
-        for(int i=0;i<n;i++)
+        unordered_map<int,int>m;
+        for(auto it : nums)
         {
-            for(int j=i+1;j<n;j++)
-            {
-                if(nums[i]==nums[j])
-                    s++;
-            }
+            m[it]++;
+        }
+        
+        for(auto it : m )
+        {
+            int x=it.second;
+            s+=x*(x-1)/2;
         }
         return s;
     }
