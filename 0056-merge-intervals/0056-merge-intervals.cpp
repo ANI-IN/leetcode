7134b4f420c7cd26de80512b56bc114993a4cc19
ANI-IN <44803072+ANI-IN@users.noreply.github.com>
2023-08-22 10:26:36 +0530

    Time: 27 ms (91.45%), Space: 18.9 MB (71.17%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& a) {
+        int n=a.size();
+        sort(a.begin(),a.end());
+        vector<vector<int>> ans;
+        ans.push_back(a[0]);
+        
+        for(int i=1;i<n;i++)
+        {
+            int start=a[i][0];
+            int end=a[i][1];
+            
+            if(ans.back()[1]<start)
+                ans.push_back(a[i]);
+            else
+            {
+                ans.back()[0]=min(ans.back()[0],start);
+                ans.back()[1]=max(ans.back()[1],end);
+            }
+        }
+        return ans;
+    }
+};
