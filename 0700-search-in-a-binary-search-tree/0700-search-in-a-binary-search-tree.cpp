4cc8a1bfcc4b87f46b34b3b1470b7629ffbe112d
ANI-IN <44803072+ANI-IN@users.noreply.github.com>
2023-08-24 10:29:31 +0530

    Time: 27 ms (93.02%), Space: 34.9 MB (10.02%) - LeetHub

--- 0700-search-in-a-binary-search-tree/0700-search-in-a-binary-search-tree.cpp
+++ 0700-search-in-a-binary-search-tree/0700-search-in-a-binary-search-tree.cpp
@@ -13,15 +13,11 @@ class Solution {
 public:
     TreeNode* searchBST(TreeNode* root, int val) {
         
-        if(root==NULL)
-            return NULL;
-        
-        if(root->val==val)
-            return root;
-        if(root->val < val)
-            return searchBST(root->right,val);
-        
-           return searchBST(root->left,val);
+       while(root!=NULL && root->val!=val)
+       {
+           root=val > root->val?root->right:root->left;
+       }
+        return root;
        
     
     }
