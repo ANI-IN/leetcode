e5579908831ca02e36124e42a276dc30d1202d49
ANI-IN <44803072+ANI-IN@users.noreply.github.com>
2023-08-31 09:31:23 +0530

    Time: 15 ms (88.73%), Space: 13.8 MB (64.89%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    vector<int> findOrder(int n, vector<vector<int>>& graph) {
+        
+        vector<int> adj[n];
+        vector<int> indegree(n,0);
+        
+        for(auto it : graph)
+        {
+            int u=it[0];
+            int v=it[1];
+            
+            adj[v].push_back(u);
+            indegree[u]++;
+        }
+        
+        queue<int> q;
+        for(int i=0;i<n;i++)
+        {
+            if(indegree[i]==0)
+                q.push(i);
+        }
+        vector<int> ans;
+        while(!q.empty())
+        {
+            int node=q.front();
+            q.pop();
+            ans.push_back(node);
+            
+            for(auto it : adj[node])
+            {
+                indegree[it]--;
+                if(indegree[it]==0)
+                {
+                    q.push(it);
+                }
+            }
+        }
+        
+        if(ans.size()==n)
+            return ans;
+        else
+            return {};
+    }
+};
