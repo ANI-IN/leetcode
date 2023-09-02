11a3a07f91956839365fa482a26e44d64c1b8c5f
ANI-IN <44803072+ANI-IN@users.noreply.github.com>
2023-09-02 08:49:49 +0530

    Time: 419 ms (20.34%), Space: 207.3 MB (12.61%) - LeetHub

---
+++ 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.cpp
@@ -0,0 +1,39 @@
+class Solution {
+    int solve(int index,string &s, unordered_map<string,int>&mpp,vector<int> &dp )
+    {
+        if(index>=s.size())
+            return 0;
+        
+        if(dp[index]!=-1)
+            return dp[index];
+        
+        int ans=s.size();
+        string str="";
+        for(int i=index;i<s.size();i++)
+        {
+            str.push_back(s[i]);
+            int count=str.size();
+            if(mpp[str])
+                count=0;
+            
+            count=count+solve(i+1,s,mpp,dp);
+            ans=min(count,ans);
+                
+        }
+        return dp[index]=ans;
+        
+    }
+public:
+    int minExtraChar(string s, vector<string>& dictionary) {
+        int n=s.size();
+        unordered_map<string,int> mpp;
+        vector<int> dp(n,-1);
+        
+        
+        for(auto it : dictionary)
+        {
+            mpp[it]++;
+        }
+        return solve(0,s,mpp,dp);
+    }
+};
