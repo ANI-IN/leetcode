72722bd57f4e98b7b6472b88f375986b4dc17713
ANI-IN <44803072+ANI-IN@users.noreply.github.com>
2023-09-08 11:41:13 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (82.50%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -1,19 +1,16 @@
 class Solution {
 public:
-    vector<vector<int>> generate(int n) {
-        vector<vector<int>> ans;
-        
+    vector<vector<int>> generate(int numRows) {
+       
+        int n=numRows;
+         vector<vector<int>> ans(n);
         for(int i=0;i<n;i++)
         {
-           vector<int> temp;
-            for(int j=0;j<=i;j++)
+            ans[i]=vector<int> (i+1,1);
+            for(int j=1;j<i;j++)
             {
-                if(j==0 || i==j)
-                    temp.push_back(1);
-                else
-                    temp.push_back(ans[i-1][j-1]+ans[i-1][j]);
+                ans[i][j]=ans[i-1][j]+ans[i-1][j-1];
             }
-            ans.push_back(temp);
         }
         return ans;
     }
