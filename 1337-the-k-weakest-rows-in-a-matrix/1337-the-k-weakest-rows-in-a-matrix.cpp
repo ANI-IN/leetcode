ad375fd413d34ce41bab9d1d93d6f235b9c7b302
ANI-IN <44803072+ANI-IN@users.noreply.github.com>
2023-09-18 21:00:14 +0530

    Time: 14 ms (14.22%), Space: 10.9 MB (9.15%) - LeetHub

--- 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.cpp
+++ 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.cpp
@@ -4,9 +4,10 @@ public:
         int n=mat.size();
         int m=mat[0].size();
         
-        priority_queue<pair<int,int>>q;
+        priority_queue< pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>>q;
+        
         for(int i=0;i<n;i++)
-        {   
+        {
             int count=0;
             for(int j=0;j<m;j++)
             {
@@ -14,19 +15,18 @@ public:
                     count++;
             }
             q.push({count,i});
+             
             
-            if(q.size()>k)
-                q.pop();
         }
         
         vector<int> ans;
-        while(!q.empty())
+        
+        while(k--)
         {
-            int index=q.top().second;
-            ans.push_back(index);
+            ans.push_back(q.top().second);
             q.pop();
+            
         }
-        reverse(ans.begin(),ans.end());
         return ans;
     }
 };
