117f8bf2ef534d22c113ffeb1c4c510e1910863b
ANI-IN <44803072+ANI-IN@users.noreply.github.com>
2023-09-21 17:07:04 +0530

    Time: 24 ms (69.42%), Space: 89.7 MB (37.84%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,46 +1,69 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        
         int n=nums1.size();
         int m=nums2.size();
-        double ans=0.0;
         
         int len=n+m;
-        vector<int> a;
+        int e1,e2,k=0;
+        
+        int pos1,pos2;
+        
+        pos1=len/2;
+        pos2=len/2-1;
+        
         int i=0,j=0;
         
         while(i<n && j<m)
         {
             if(nums1[i] < nums2[j])
             {
-                a.push_back(nums1[i]);
-                i++;
+                if(pos1==k)
+                    e1=nums1[i];
+                if(pos2==k)
+                    e2=nums1[i];
+                i++;        
             }
             else
             {
-                a.push_back(nums2[j]);
+                if(pos1==k)
+                    e1=nums2[j];
+                if(pos2==k)
+                    e2=nums2[j];
                 j++;
             }
+            k++;
         }
         
         while(i<n)
         {
-            a.push_back(nums1[i++]);
-        }
-        while(j<m)
-        {
-            a.push_back(nums2[j++]);
+            if(pos1==k)
+                    e1=nums1[i];
+            if(pos2==k)
+                    e2=nums1[i];
+            i++;
+            k++;   
         }
         
+        while(j<m)
+        {
+            if(pos1==k)
+                    e1=nums2[j];
+            if(pos2==k)
+                    e2=nums2[j];
+            j++;
+            k++;      
+        }
+        
+        double ans=0.0;
         if(len%2==1)
         {
-            ans=a[len/2];
+            ans=e1;
             return ans;
         }
         else
         {
-            ans=(a[len/2]+a[len/2-1])/2.0;
+            ans=(e1+e2)/2.0;
             return ans;
         }
     }
