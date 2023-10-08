1e9c2593f3ed7c09d68f0e2acc2d9fc5686c916a
ANI-IN <44803072+ANI-IN@users.noreply.github.com>
2023-10-09 01:56:42 +0530

    Time: 47 ms (34.97%), Space: 13.7 MB (24.09%) - LeetHub

--- 1458-max-dot-product-of-two-subsequences/1458-max-dot-product-of-two-subsequences.cpp
+++ 1458-max-dot-product-of-two-subsequences/1458-max-dot-product-of-two-subsequences.cpp
@@ -1,52 +1,35 @@
 class Solution {
-    int solve(int i, int j, int n, int m,vector<int>& nums1,vector<int>& nums2,vector<vector<int>> &dp)
+    
+    vector<vector<int>> dp;
+    int md = 1e9+7;
+    int n, m;
+    
+    int solve(int i, int j, vector<int>& nums1, vector<int>& nums2)
     {
-        if(i==n || j==m)
+        if(i == n || j == m)
             return -1e9;
         
-        if(dp[i][j]!=-1e9)
+        if(dp[i][j] != -1)
             return dp[i][j];
         
-        int val=nums1[i]*nums2[j];
+        int val = nums1[i] * nums2[j];
         
-        int take=nums1[i]*nums2[j]+solve(i+1,j+1,n,m,nums1,nums2,dp);
+        int take = val + solve(i + 1, j + 1, nums1, nums2);
         
-        int take_i=solve(i,j+1,n,m,nums1,nums2,dp);
+        int take_i = solve(i, j + 1, nums1, nums2);
         
-        int take_j=solve(i+1,j,n,m,nums1,nums2,dp);
+        int take_j = solve(i + 1, j, nums1, nums2);
         
-        return dp[i][j]=max({val,take,take_i,take_j});
-        
-        
+        return dp[i][j] = max({val, take, take_i, take_j});
     }
 public:
     int maxDotProduct(vector<int>& nums1, vector<int>& nums2) {
-        int n = nums1.size();
-        int m = nums2.size();
+        n = nums1.size();
+        m = nums2.size();
+        dp.assign(n, vector<int>(m, -1));
+        return solve(0, 0, nums1, nums2);
+
         
-        // Create a dp table
-        vector<vector<int>> dp(n + 1, vector<int>(m + 1, -1e9));
         
-        // Initialize the dp table for the base case
-        for (int i = 0; i <= n; i++) {
-            for (int j = 0; j <= m; j++) {
-                if (i == 0 || j == 0) {
-                    dp[i][j] = -1e9;
-                }
-            }
-        }
-        
-        // Fill in the dp table
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= m; j++) {
-                int val = nums1[i - 1] * nums2[j - 1];
-                
-                // Calculate the maximum dot product by considering various options
-                dp[i][j] = max({val, dp[i - 1][j - 1] + val, dp[i - 1][j], dp[i][j - 1]});
-            }
-        }
-        
-        // The result is stored in dp[n][m]
-        return dp[n][m];
     }
 };
