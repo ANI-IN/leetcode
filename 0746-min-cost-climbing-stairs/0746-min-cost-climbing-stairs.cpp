b336b5441c505d376b79787f6f15e84e4fb9a236
ANI-IN <44803072+ANI-IN@users.noreply.github.com>
2023-10-13 17:38:41 +0530

    Time: 9 ms (19.87%), Space: 13.9 MB (37.30%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -1,18 +1,4 @@
 class Solution {
-    
-    int solve(int index,vector<int>& cost, vector<int>&dp )
-    {
-        if(index>=cost.size())
-            return 0;
-        
-        if(dp[index]!=-1)
-            return dp[index];
-        
-        int one=cost[index]+solve(index+1,cost,dp);
-        int two=cost[index]+solve(index+2,cost,dp);
-        
-        return dp[index]=min(one,two);
-    }
 public:
     int minCostClimbingStairs(vector<int>& cost) {
         
@@ -27,10 +13,6 @@ public:
             y=current;
         }
         
-        return min(x,y);
-        
-         
-        
-        
+        return min(x,y);   
     }
 };
