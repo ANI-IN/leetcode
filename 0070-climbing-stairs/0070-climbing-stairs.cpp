0bd28673d18491607c7c039249ab0601e5af5463
ANI-IN <44803072+ANI-IN@users.noreply.github.com>
2023-10-18 01:25:49 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (29.32%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,26 +1,18 @@
 class Solution {
-    int solve(int n, vector<int>&dp)
-    {
-        if(n==0 || n==1)
-            return 1; 
-        
-        if(dp[n]!=-1)
-            return dp[n];
-        
-        return dp[n]=solve(n-1,dp)+solve(n-2,dp);
-    }
 public:
     int climbStairs(int n) {
         vector<int> dp(n+1,-1);
         
-        dp[0]=1;
-        dp[1]=1;
+       int prev0=1;
+       int prev1=1;
         
         for(int i=2;i<=n;i++)
         {
-            dp[i]=dp[i-1]+dp[i-2];
+            int curr=prev0+prev1;
+            prev0=prev1;
+            prev1=curr;
         }
-        return dp[n];
+        return prev1;
         
     }
 };
