b62c99a5bb68d4635e7982c8003b9b29fc6a5ed7
Animesh Kumar <44803072+ANI-IN@users.noreply.github.com>
2024-01-06 15:32:07 +0530

    Time: 64 ms (48.35%), Space: 13.6 MB (81.39%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -1,54 +1,55 @@
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        sort(nums.begin(),nums.end());
         int n=nums.size();
         vector<vector<int>> ans;
-        sort(nums.begin(),nums.end());
         
         for(int i=0;i<n;i++)
         {
             if(i>0 && nums[i]==nums[i-1])
                 continue;
-            
             for(int j=i+1;j<n;j++)
             {
-                if(j!=(i+1) && nums[j]==nums[j-1])
+                if(j!=i+1 && nums[j]==nums[j-1])
                     continue;
-                
                 int k=j+1;
-                int z=n-1;
+                int h=n-1;
                 
-                while(k<z)
-                {   
+                while(k<h)
+                {
                     long long sum=nums[i];
                     sum+=nums[j];
                     sum+=nums[k];
-                    sum+=nums[z];
+                    sum+=nums[h];
+                    
                     if(sum<target)
                     {
                         k++;
                     }
                     else if(sum>target)
                     {
-                        z--;
+                        h--;
                     }
                     else
                     {
-                        vector<int> temp={nums[i],nums[j],nums[k],nums[z]};
+                        vector<int> temp{nums[i],nums[j],nums[k],nums[h]};
                         ans.push_back(temp);
                         
                         k++;
-                        z--;
+                        h--;
                         
-                        while(k<z && nums[k]==nums[k-1])
+                        while(k<h && nums[k]==nums[k-1])
                             k++;
-                        while(z>k && nums[z]==nums[z+1])
-                            z--;
+                        
+                        while(h>k && nums[h]==nums[h+1])
+                            h--;
                     }
                 }
             }
-            
+           
+                
         }
-        return ans;
+         return ans;
     }
 };
