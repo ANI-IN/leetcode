fd6e1cd6baf00dfabc46a2330dca18e94479f088
Animesh Kumar <44803072+ANI-IN@users.noreply.github.com>
2024-02-05 22:13:23 +0530

    Time: 16 ms (51.03%), Space: 10.6 MB (10.18%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,42 +1,47 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        int m=s.size(), n=t.size();
-        unordered_map<char, int> mp;
+        int i=0,j=0,n=s.size();
         
-        int ans = INT_MAX; 
-        int start = 0; 
+        unordered_map<char,int> m;
         
-        for(auto x:t)
-            mp[x]++;
+        int start=0,ans=INT_MAX;
         
-        int count = mp.size();
+        string str="";
         
-        int i = 0, j = 0;
-
-        while (j < s.length()) {
-            mp[s[j]]--;
-            if (mp[s[j]] == 0)
-                count--;
-
-            if (count == 0) {
-                while (count == 0) {
-                    if (ans > j - i + 1) {
-                        ans = j - i + 1;
-                        start = i;
+        for(auto it : t)
+            m[it]++;
+        
+        int count=m.size();
+        while(j<n)
+        {
+            m[s[j]]--;
+            
+            if(m[s[j]]==0)
+            count--;
+            if(count==0)
+            {
+                while(count==0)
+                {
+                    if(ans>j-i+1)
+                    {
+                        ans=j-i+1;
+                        start=i;
                     }
-                    mp[s[i]]++;
-                    if (mp[s[i]] > 0)
+                    
+                    m[s[i]]++;
+                    if(m[s[i]]>0)
                         count++;
-
+                    
                     i++;
                 }
             }
             j++;
+            
         }
-        if (ans != INT_MAX)
-            return s.substr(start, ans);
-        else
+        if(ans==INT_MAX)
             return "";
+        else
+            return s.substr(start,ans);
     }
 };
