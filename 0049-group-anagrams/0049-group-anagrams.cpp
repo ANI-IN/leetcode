ae1d77882c5c67b1da137e2d72ba3eb84ae50576
Animesh Kumar <44803072+ANI-IN@users.noreply.github.com>
2024-02-06 21:19:51 +0530

    Time: 36 ms (35.13%), Space: 29 MB (8.82%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,20 +1,38 @@
 class Solution {
+    string solve(string str)
+    {
+        vector<int> arr(26,0);
+        for(int i=0;i<str.size();i++)
+        {
+            arr[str[i]-'a']++;
+        }
+        
+        string ans="";
+        
+        for(int i=0;i<26;i++)
+        {
+            if(arr[i]>0)
+                ans+= string(arr[i], i+'a');
+        }
+        return ans;
+            
+    }
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        vector<vector<string>> ans;
         int n=strs.size();
         unordered_map<string,vector<string>> m;
         
         for(auto it : strs)
         {
-            string t=it;
-            sort(t.begin(),t.end());
-            m[t].push_back(it);
+            string s=solve(it);
+            m[s].push_back(it);
         }
         
+        vector<vector<string>> ans;
         for(auto it : m)
         {
             ans.push_back(it.second);
+            
         }
         return ans;
     }
