3e43c3507acfc0d0357b5858149cf31944bf44a7
Animesh Kumar <44803072+ANI-IN@users.noreply.github.com>
2024-02-08 23:20:09 +0530

    Time: 77 ms (73.58%), Space: 11.3 MB (22.32%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.cpp
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -6,17 +6,19 @@ class Solution {
         
         if(dp[n]!=-1)
             return dp[n];
+        
         int ans=n;
         for(int i=1;i*i<=n;i++)
         {
+            
             ans=min(ans,1+solve(n-i*i,dp));
         }
+        
         return dp[n]=ans;
     }
 public:
     int numSquares(int n) {
         vector<int> dp(n+1,-1);
-        
         return solve(n,dp);
     }
 };
