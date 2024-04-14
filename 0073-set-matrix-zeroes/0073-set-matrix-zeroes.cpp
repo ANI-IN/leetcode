3653ef0ac52b7a63f9b2a461aff9aae8a422e25d
Animesh Kumar <44803072+ANI-IN@users.noreply.github.com>
2024-04-14 16:38:19 +0530

    Time: 8 ms (71.88%), Space: 16.4 MB (14.54%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -3,6 +3,7 @@ public:
     void setZeroes(vector<vector<int>>& matrix) {
         int n=matrix.size();
         int m=matrix[0].size();
+        
         vector<int> row(n,-1);
         vector<int> col(m,-1);
         
@@ -18,16 +19,16 @@ public:
             }
         }
         
-        
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<m;j++)
             {
                 if(row[i]==1 || col[j]==1)
-                {
                     matrix[i][j]=0;
-                }
             }
         }
+        
+        
+        
     }
 };
