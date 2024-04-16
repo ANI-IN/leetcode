fb89c3c11ec64f3e9e8adbabfaa4dd1976a3bb2e
Animesh Kumar <44803072+ANI-IN@users.noreply.github.com>
2024-04-16 19:40:36 +0530

    Time: 29 ms (78.03%), Space: 24.4 MB (98.30%) - LeetHub

--- 1283-find-the-smallest-divisor-given-a-threshold/1283-find-the-smallest-divisor-given-a-threshold.cpp
+++ 1283-find-the-smallest-divisor-given-a-threshold/1283-find-the-smallest-divisor-given-a-threshold.cpp
@@ -1,37 +1,28 @@
 class Solution {
-    double solve(vector<int>&nums, int d)
-    {
-        int n=nums.size();
-        double ans=0;
-        
-        for(int i=0;i<n;i++)
-        {
-            ans=ans+ceil((double)nums[i]/(double)d);
+    double calculateSum(vector<int>& nums, int divisor) {
+        double sum = 0;
+        for (int num : nums) {
+            sum += ceil((double)num / divisor);
         }
-        return ans;
+        return sum;
     }
+    
 public:
     int smallestDivisor(vector<int>& nums, int threshold) {
-        int n=nums.size();
-        int low=1;
-        int high=*max_element(nums.begin(),nums.end());
+        int low = 1;
+        int high = *max_element(nums.begin(), nums.end());
         
-        
-        while(low<=high)
-        {
-            int mid=low+(high-low)/2;
-            double divisor=solve(nums,mid);
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            double currentSum = calculateSum(nums, mid);
             
-            if(divisor<=threshold)
-            {
-                high=mid-1;
-            }
-            else
-            {
-                low=mid+1;
+            if (currentSum <= threshold) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
             }
         }
+        
         return low;
-            
     }
 };
