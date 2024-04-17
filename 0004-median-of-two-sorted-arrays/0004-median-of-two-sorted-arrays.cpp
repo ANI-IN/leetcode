db86356b9c25dc39656373c3b3aa80d7ab3421e9
Animesh Kumar <44803072+ANI-IN@users.noreply.github.com>
2024-04-17 18:03:12 +0530

    Time: 27 ms (50.85%), Space: 94.4 MB (75.74%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -4,46 +4,60 @@ public:
         int n=nums1.size();
         int m=nums2.size();
         double ans=0.0;
-        vector<int> a;
+      
         int len=n+m;
+        
+        int index1=len/2;
+        int index2=index1-1;
+        int e1=-1,e2=-1;
+        int count=0;
+        
         
         int i=0,j=0;
         
         while(i<n && j<m)
         {
             if(nums1[i]<nums2[j])
-            {
-                a.push_back(nums1[i]);
+            {   
+                if(count==index1) e1=nums1[i];
+                if(count==index2) e2=nums1[i];
+                count++;
                 i++;
             }
             else
-            {
-                a.push_back(nums2[j]);
+            {   
+                if(count==index1) e1=nums2[j];
+                if(count==index2) e2=nums2[j];
+                count++;
                 j++;
             }
         }
         
         while(i<n)
         {
-            a.push_back(nums1[i]);
+            if(count==index1) e1=nums1[i];
+            if(count==index2) e2=nums1[i];
+            count++;
             i++;
                 
         }
         
         while(j<m)
         {
-            a.push_back(nums2[j]);
+            if(count==index1) e1=nums2[j];
+            if(count==index2) e2=nums2[j];
+            count++;
             j++;
         }
         
         
         if(len%2==1)
         {
-            ans=a[len/2];
+            ans=e1;
         }
         else
         {
-            ans=(a[len/2]+a[len/2-1])/2.0;
+            ans=(double)(e1+e2)/2.0;
         }
         
         return ans;
