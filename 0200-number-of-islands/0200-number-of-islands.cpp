d30f4af75add77f4c4e65ec782c0d9a7a82e6b67
Animesh Kumar <44803072+ANI-IN@users.noreply.github.com>
2024-04-19 21:01:42 +0530

    Time: 33 ms (33.65%), Space: 23.7 MB (17.66%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,15 +1,15 @@
 class Solution {
-    void solve(int i, int j, vector<vector<char>>&grid,vector<vector<int>> &vis)
-    {   
+    void bfs(int i, int j, vector<vector<char>>& grid, vector<vector<int>> &vis)
+    {
         int n=grid.size();
         int m=grid[0].size();
+        queue<pair<int,int>> q;
+        q.push({i,j});
         vis[i][j]=1;
-        grid[i][j]='0';
+        
+        
         int delrow[]={-1,0,+1,0};
         int delcol[]={0,+1,0,-1};
-        queue<pair<int,int>>q;
-        q.push({i,j});
-        
         
         while(!q.empty())
         {
@@ -17,38 +17,39 @@ class Solution {
             int col=q.front().second;
             q.pop();
             
-            
             for(int i=0;i<4;i++)
             {
                 int nrow=delrow[i]+row;
                 int ncol=delcol[i]+col;
                 
-                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && grid[nrow][ncol]=='1' && vis[nrow][ncol]==0)
-                {   
+                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && !vis[nrow][ncol] && grid[nrow][ncol]=='1')
+                {
                     vis[nrow][ncol]=1;
                     q.push({nrow,ncol});
                 }
             }
         }
-        
     }
 public:
     int numIslands(vector<vector<char>>& grid) {
         int n=grid.size();
         int m=grid[0].size();
+        
         vector<vector<int>> vis(n,vector<int>(m,0));
-        int count=0;
+        int island=0;
+        
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<m;j++)
             {
                 if(grid[i][j]=='1' && vis[i][j]==0)
                 {
-                    count++;
-                    solve(i,j,grid,vis);
+                    island++;
+                    bfs(i,j,grid,vis);
                 }
             }
         }
-        return count;
+        
+        return island;
     }
 };
