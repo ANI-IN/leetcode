ac0ee8526907d76255473c9c249a391ff2809222
Animesh Kumar <44803072+ANI-IN@users.noreply.github.com>
2024-04-24 20:14:20 +0530

    Time: 8 ms (72.60%), Space: 22.4 MB (52.50%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -2,31 +2,42 @@ class Solution {
 public:
     int trap(vector<int>& arr) {
         int n=arr.size();
-        vector<int> pre(n);
-        vector<int> suff(n);
+        int ans=0;
+        int prefix[n];
+        int sufix[n];
         
-        pre[0]=arr[0];
-        
+        prefix[0]=arr[0];
         for(int i=1;i<n;i++)
         {
-            pre[i]=max(pre[i-1],arr[i]);
+            if(prefix[i-1]<arr[i])
+            {
+                prefix[i]=arr[i];
+            }
+            else
+            {
+                prefix[i]=prefix[i-1];
+            }
         }
         
-        suff[n-1]=arr[n-1];
+        
+        sufix[n-1]=arr[n-1];
         for(int i=n-2;i>=0;i--)
         {
-            suff[i]=max(suff[i+1],arr[i]);
+            if(sufix[i+1] < arr[i])
+            {
+                sufix[i]=arr[i];
+            }
+            else
+            {
+                sufix[i]=sufix[i+1];
+            }
         }
         
-        
-        int ans=0;
         for(int i=0;i<n;i++)
         {
-            ans+=min(pre[i],suff[i])-arr[i];
+            ans=ans+min(prefix[i],sufix[i])-arr[i];
         }
         return ans;
-            
-            
-            return 0;
+        
     }
 };
